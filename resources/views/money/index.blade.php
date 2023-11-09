688ea7afb5ec2a853606112b882cfc8abacaa87d
池フクロウ <pugachev2011@gmail.com>
2023-11-09 16:27:39 +0900

    favicon設定

--- resources/views/money/index.blade.php
+++ resources/views/money/index.blade.php
@@ -20,6 +20,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    <link rel="shortcut icon" href="{{ asset('/money.png') }}">
     <title>MoneyWars</title>
   </head>
   <body>
