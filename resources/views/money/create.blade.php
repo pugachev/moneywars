3fd7788ad492fb45ddd755a84ec8c36c58f51eb4
池フクロウ <pugachev2011@gmail.com>
2023-11-09 16:10:51 +0900

    fadeOut表示処理

--- resources/views/money/create.blade.php
+++ resources/views/money/create.blade.php
@@ -48,7 +48,7 @@
         </form>
     </div>
     <!-- ここに本文を記述します -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="../bootstrap-datepicker/css/bootstrap-datepicker.min.css">
